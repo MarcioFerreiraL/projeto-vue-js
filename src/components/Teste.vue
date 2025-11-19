85dfe362392e1512a6940696a9cc4225d720699b
MarcioFerreiraL <contactmarcioflima@gmail.com>
2025-11-19 12:03:20 -0300

    hello

---
+++ src/components/Teste.vue
@@ -0,0 +1,18 @@
+<template>
+    <Teste class="teste">
+        teste
+    </Teste>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .teste {
+        font-family: 'Courier New', Courier, monospace;
+        color: blue;
+    }
+</style>
